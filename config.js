18e30519e0556d6f89fb864463f86307d4d5726a
Alexei <alexeibradshaw12@gmail.com>
2022-04-17 02:55:23 +0100

    Created prompt() to get number from user, edited drawboxes() to change gridTemplat figures

--- config.js
+++ config.js
@@ -5,34 +5,43 @@ square.setAttribute('class', 'square');
 
 
 let drawBoxes = function (userNumber = 64) {
-    if((userNumber <= 100) && (userNumber >= 0)) {
-        for(let i = 0; i < userNumber; i++) {
-            const containerSelect = document.querySelector('.container');
-            const square = document.createElement('div');
-            square.setAttribute('class', 'square');
-            containerSelect.appendChild(square); 
+    const containerSelect = document.querySelector('.container');
+    containerSelect.style.setProperty('grid-template-columns', `repeat(${userNumber}, 1fr)`);
+    containerSelect.style.setProperty('grid-template-rows', `repeat(${userNumber}, 1fr)`);
+    if(containerSelect.firstChild) {
+        while(containerSelect.firstChild) {
+            containerSelect.removeChild(containerSelect.firstChild);
         }
-        const squareSelect = document.getElementsByClassName('square');
-        for(i = 0; i< squareSelect.length; i ++) {
-            squareSelect[i].addEventListener('mouseover', function(e) {
-                squareSelect[i].classList.toggle('squareHover')
-    })};
+        }
+
+    for(let i = 0; i < (userNumber * userNumber); i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        containerSelect.appendChild(square); 
+    };
+
+    const squareSelect = document.getElementsByClassName('square');
+    [...squareSelect].forEach(element => {
+        element.addEventListener('mouseover', (e)=>{
+            element.classList.toggle('squareHover');
+        })
+    })
+    
+};
+
+drawBoxes(64);
+
+let prompt = function() {
+    let returnNum = window.prompt("Enter a value below 100");
+    if((returnNum <= 100) && (returnNum >= 0)) {
+        drawBoxes(returnNum);
     } else {
         alert("Enter a number between 0 and 100!");
-        
-    }  
+    }
+   
 };
-drawBoxes();
-
-const squareSelect = document.getElementsByClassName('square');
-for(let i = 0; i< squareSelect.length; i ++) {
-    squareSelect[i].addEventListener('mouseover', function(e) {
-        squareSelect[i].classList.toggle('squareHover')
-    })};
-
-
-prompt = () => window.prompt("Enter a value below 100");
 
 const buttonSelect = document.querySelector(".topButton");
+buttonSelect.addEventListener('click', prompt);
 
 
